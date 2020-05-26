2bd947af04d78c149b2de8e3b419b9d8bb854ae4
yrachid <anuar.yasser@gmail.com>
2020-05-26 14:31:38 -0300

    All bash functions in a single script

--- shlides.sh
+++ shlides.sh
@@ -1,36 +1,131 @@
 #!/bin/bash -eu
 
+CURRENT_SLIDE=0
+
 readonly SLIDES_LOCATION="${1:-${PWD}/sample-slides}"
 
-source ./controls.sh
+transition() {
+  tput reset
+}
 
-start() {
-  clear
+quit_presentation() {
+  transition
+  exit 0
+}
 
-  printf "\n\nSHLIDES\n\n"
-  printf "About the presentation:\n"
-  printf "  - slides location: $(slides_location)\n"
-  printf "  - slide count: $(slide_count)\n"
-  printf "  - slide list: $(inline_slide_list | tr '\n' ' ')\n"
-  printf "\n"
-  printf "Get started:\n"
-  printf "  s: present from start\n"
-  printf "  q: quit\n"
-  printf "\n"
+slides_location() {
+  printf "${SLIDES_LOCATION}"
+}
+
+slide_count() {
+  ls -1 $(slides_location) | wc -l | sed 's/ //g'
+}
+
+slide_list() {
+  ls $(slides_location) | sort
+}
+
+inline_slide_list() {
+  slide_list | tr '\n' ' '
+}
+
+current_slide() {
+  slide_list | sed -n "${CURRENT_SLIDE}p"
+}
+
+present() {
+  local slide_to_present=${1:-1}
+  transition
+  $(slides_location)/$slide_to_present
+  wait_for_command
+}
+
+next_slide() {
+  if [ "$CURRENT_SLIDE" -lt "$(slide_count)" ]; then
+    CURRENT_SLIDE=$((CURRENT_SLIDE + 1))
+  fi
+
+  present "$(current_slide)"
+}
+
+previous_slide() {
+  if [ ! "$CURRENT_SLIDE" -eq "1" ]; then
+    CURRENT_SLIDE=$((CURRENT_SLIDE - 1))
+  fi
+
+  present "$(current_slide)"
+}
+
+readonly GREETING=$(cat <<- EOM
+
+_____________________
+...::: SHLIDES :::...
+_____________________
+
+About the presentation:
+ - slides location: $(slides_location)
+ - slide count: $(slide_count)
+ - slide list: $(inline_slide_list | tr '\n' ' ')
+
+Get started:
+  s: present from start
+  q: quit
+EOM
+)
+
+readonly INSTRUCTIONS=$(cat <<- EOM
+Instructions:
+  j: next slide
+  k: prev slide
+  q: quit
+
+Press any key to start
+EOM
+)
+
+start_presentation() {
+  transition
+  printf "$INSTRUCTIONS"
+  read -n1 whatever
+  next_slide
+}
+
+wait_for_command() {
+  read -n1 cmd
+  case $cmd in
+    j)
+      next_slide
+      ;;
+    k)
+      previous_slide
+      ;;
+    q)
+      quit_presentation
+      ;;
+    *)
+      wait_for_command
+      ;;
+  esac
+}
+
+main() {
+  transition
+
+  printf "$GREETING\n"
+
   read -p "> " -n1 input
 
   case $input in
     q)
-      clear
-      exit 0
+      quit_presentation
       ;;
     s)
       start_presentation
       ;;
     *)
-      start
+      main
       ;;
   esac
 }
 
-start
+main
